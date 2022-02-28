391ea90fa650632c90b3183f7e72b51657814f0d
danifullstack <danifullstackengineer@gmail.com>
2022-02-28 05:33:44 +0100

    building app

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import router from './routes/tasks.js';
+import path from 'path';
 
 dotenv.config()
 const app = express();
@@ -13,13 +14,22 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/', router)
 
+// Serving static assets if in prod
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'))
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 mongoose
 .connect(process.env.DB_URL)
 .then(() => {
     console.log('Connected to db...');
-    app.listen(process.env.PORT, ()=>{
-        console.log(`Server is running on port ${process.env.PORT}`);
+    app.listen(process.env.PORT || 5000, ()=>{
+        console.log(`Server is running on port ${process.env.PORT || 5000}`);
     })
 })
 .catch(err => {

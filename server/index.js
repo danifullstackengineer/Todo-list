84f76441f34715417e13179917266b93edafb15b
danifullstack <danifullstackengineer@gmail.com>
2022-02-28 08:46:57 +0100

    changed imports

--- server/index.js
+++ server/index.js
@@ -1,15 +1,17 @@
-import cors from 'cors';
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import router from './routes/tasks.js';
-import path from 'path';
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotnev');
+const bodyParser = require('body-parser');
+const router = require('./routes/tasks.js');
+const path = require('path');
 
 dotenv.config()
 const app = express();
 
-app.use(cors());
+if(!process.env.NODE_ENV){
+    app.use(cors());
+}
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use('/', router)

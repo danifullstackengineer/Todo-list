2e684d86a732a6905b140de9f2807aee73f713de
danifullstack <danifullstackengineer@gmail.com>
2022-03-12 23:24:40 +0100

    modified index.js

--- server/index.js
+++ server/index.js
@@ -23,10 +23,10 @@ app.use('/', router)
 // Serving static assets if in prod
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static(join(__dirname, "..", "client", "build")))
+    app.use(express.static(path.join(__dirname, "..", "client", "build")))
 
     app.get('*', (req,res) => {
-        res.sendFile(resolve(__dirname,'..' ,'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname,'..' ,'client', 'build', 'index.html'));
     })
 }
 
